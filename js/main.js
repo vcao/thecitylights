cb18b7eb9f962044e087952e2e3a650b861f4248
Victor Cao <code@victorcao.com>
2013-11-13 12:22:00 -0800

    Fixed BigVideo dependencies

--- js/main.js
+++ js/main.js
@@ -1,28 +1,23 @@
 require.config({
-	paths: {
-		'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-		'BigVideo': 'BigVideo.js/lib/bigvideo',
-		'jquery-ui': 'jquery-ui/ui/jquery-ui',
-		'videojs': 'video.js/video',
-		'imagesloaded': 'imagesloaded/imagesloaded',
-		'eventEmitter/EventEmitter': 'eventEmitter/EventEmitter',
-		'eventie/eventie': 'eventie/eventie'
-	},
-	shim: {
-		"videojs": {exports: 'videojs'}
-	}
-});
-
-require(['order!jquery', 'order!BigVideo'], function ($) {
-	$(function() {
-		var BV = new $.BigVideo({useFlashForFirefox:false});
-		BV.init();
-		if (Modernizr.touch) {
-		    BV.show('img/fireworks.jpg');
-		} else {
-		    BV.show('img/bg_tcl.mp4',{altSource:'img/bg_tcl.ogv',ambient:true});
+		paths: {
+			'BigVideo': 'BigVideo.js/lib/bigvideo',
+			'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+			'jquery-ui': 'jquery-ui/ui/jquery-ui',
+			'videojs': 'video.js/video',
+			'imagesloaded': 'imagesloaded/imagesloaded',
+			'eventEmitter/EventEmitter': 'eventEmitter/EventEmitter',
+			'eventie/eventie': 'eventie/eventie'
+		},
+		shim: {
+			'videojs': {
+				deps: ['jquery'],
+				exports: 'videojs'
+		},
+			'BigVideo': {
+				deps: ['jquery'],
+				exports: 'BigVideo'
 		}
-	});
+	}
 });
 
 require(['jquery'], function ($) {
@@ -35,3 +30,16 @@ require(['jquery'], function ($) {
 		});
 	});
 });
+
+require(['jquery', 'jquery-ui', 'BigVideo'], function (jQuery) {
+	$(function() {
+		var BV = new $.BigVideo({useFlashForFirefox:false});
+		BV.init();
+		if (Modernizr.touch) {
+		    BV.show('img/fireworks.jpg');
+		} else {
+		    BV.show('img/bg_tcl.mp4',{altSource:'img/bg_tcl.ogv',ambient:true});
+		}
+	});
+});
+
